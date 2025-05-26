d1d715166d22fd4a83c41b5a85577bf473a2edc6
Satcom <47340064+Satcomx00-x00@users.noreply.github.com>
2025-05-26 19:38:50 +0000

    update

--- recompile.sh
+++ recompile.sh
@@ -1,1 +1,2 @@
 ollama create assistant -f "assistant.modelfile"
+ollama create qwen2.5-translator -f "translator.modelfile"
--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 ollama pull nomic-embed-text:latest
 ollama pull llama3.2:3b
+ollama pull qwen2.5:7b
 # ollama pull llama3.3:70b
 
 ollama create assistant -f "assistant.modelfile"
+ollama create qwen2.5-translator -f "translator.modelfile"
