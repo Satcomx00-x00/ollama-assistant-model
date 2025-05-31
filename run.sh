24217766f72a658b7c2263efe8b7dfefa3913677
Satcom <47340064+Satcomx00-x00@users.noreply.github.com>
2025-05-31 16:37:19 +0000

    update qwen3:7b

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 # ollama pull nomic-embed-text:latest
 # ollama pull llama3.2:3b
 ollama pull qwen2.5:7b
-# ollama pull llama3.3:70b
+ollama pull qwen3:7b
 
 # ollama create assistant -f "assistant.modelfile"
 ollama create qwen2.5-translator -f "translator.modelfile"
